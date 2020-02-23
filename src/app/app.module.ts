70dd1266bdcd6de97d8bef638216ede160276082
hercemer <hercemer42@hotmail.com>
2020-02-23 19:28:59 +0100

    bump to angular 8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import 'polyfills'
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 import { FormsModule } from '@angular/forms'
-import { HttpModule } from '@angular/http'
+import { HttpClientModule } from '@angular/http'
 
 import { AppComponent } from './app.component'
 import { HomeComponent } from './components/home/home.component'
@@ -24,7 +24,7 @@ import { UiService } from './services/ui.service'
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [ElectronService, UiService],
