c6c5807a305268518cc5d89e40fe5f8dfec5b078
Brian Fox <hercemer42@hotmail.com>
2020-05-03 15:32:29 +0200

    chore: add watch for node tests

--- test/server.js
+++ test/server.js
@@ -10,16 +10,19 @@ chai.should()
 
 describe('Array', function() {
   const pictureDirectory = basePath + '/test/assets/images'
+
   before(() => {
     return server.startStaticFileServer(pictureDirectory, config.defaults.expressJsPort)
   }) 
   
   describe('express server', function() {
-    it('should be able to request an image from the server', function() {
+    it('should be able to request an image from the server', function(done) {
       chai.request(server.app)
         .get('/t1.png')
         .end((err, res) => {
+          console.log('bueller')
           res.should.have.status(200)
+          done()
         })
     })
   })
