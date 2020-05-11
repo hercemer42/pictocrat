10aa5659cf5c37208e35d91fa17d00a8ab6d4d8f
Brian Fox <hercemer42@hotmail.com>
2020-05-11 09:51:16 +0200

    test(e2e): remove superfluous consts, arrow functions

--- e2e/main.e2e.ts
+++ e2e/main.e2e.ts
@@ -3,30 +3,28 @@ import { SpectronClient } from 'spectron';
 
 import commonSetup from './common-setup';
 
-describe('angular-electron App', function () {
+describe('angular-electron App', () => {
   commonSetup.apply(this);
 
-  let browser: any;
   let client: SpectronClient;
 
-  beforeEach(function () {
+  beforeEach(() => {
     client = this.app.client;
     browser = client as any;
   });
 
-  it('creates initial windows', async function () {
+  it('creates initial windows', async () => {
     const count = await client.getWindowCount();
     expect(count).to.equal(1);
   });
 
-  it('should get the window\'s title!', async function () {
+  it('should get the window\'s title!', async () => {
     const text = await client.getTitle()
     expect(text).to.equal('Picture Viewer');
   });
 
-  it ('should open the settings menu', async function () {
-    const menu = await client.$('div[data-tooltip="Settings"]')
-      .click()
+  it ('should open the settings menu', async () => {
+    await client.$('div[data-tooltip="Settings"]').click()
     const text = await client.$('.settings h4').getText()
     expect(text).to.equal('Settings')
   })
