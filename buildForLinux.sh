06b3758cc2689c09c4fea849369bf5a8e7c593b3
Brian Fox <hercemer42@hotmail.com>
2020-05-12 16:45:01 +0200

    did not mean to commit...

--- buildForLinux.sh
+++
@@ -1,5 +0,0 @@
-npm run electron:linux
-sudo chown root /home/brian/dev/picture-viewer/release/linux-unpacked/chrome-sandbox
-sudo chmod 4755 /home/brian/dev/picture-viewer/release/linux-unpacked/chrome-sandbox
-/home/brian/dev/picture-viewer/release/linux-unpacked/picture-viewer
-
